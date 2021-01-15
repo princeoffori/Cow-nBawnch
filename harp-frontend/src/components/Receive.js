b7572c590016c4ae26916265b13ce10d4d994e0e
Tristan Davis <tristan.samuel.davis@gmail.com>
2021-01-15 14:04:39 -0600

    Added various functionality allowing user persistance and the deletion of items on the receipt issue screen

--- harp-frontend/src/components/Receive.js
+++ harp-frontend/src/components/Receive.js
@@ -4,16 +4,19 @@ import {
   } from "react-router-dom";
 
 
-export default function Receive({receive}) {
+export default function Receive({receive, user}) {
+
+    let fitleredReceive = receive.filter( (item) => item.username === user)
+
     return(
         <>
-            <h1>Receive Item</h1>
+            <h1>Receive Items</h1>
             <div class="containter">
                 {
-                receive.map( (item) => {
+                fitleredReceive.map( (item) => {
                     return(
                         <Link className="receiveItemCntr" to={`/receive/${item.id}`}>
-                            <button className="receiveItemButton btn btn-primary">For user: {item.username} | Receipt Number: {item.id} | Items: {item.receipt[0].nomenclature}...</button>
+                            <button className="receiveItemButton btn btn-primary">Issuer: {item.issuer} | Receipt Number: {item.id} | Items: {item.receipt[0].nomenclature}...</button>
                         </Link>
                     )
                 })}
